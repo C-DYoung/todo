7ac5949c5bf6f03dcde0ef8cac4027141865dfd2
C-DYoung <rbrbdk12@gmail.com>
2026-02-07 14:04:19 +0900

    todo.d.ts파일 내용 수정

--- src/types/todo.d.ts
+++ src/types/todo.d.ts
@@ -1,12 +1,11 @@
 // 파일명.d.ts 로 설정할 경우 따로 import 없이 해당 타입 사용가능.
 
 interface ITodoItemContent {
-  content: string;
+  content: string; // todo의 내용 확인용
 }
 
 interface ITodoItem extends ITodoItemContent {
-  id: string;
-  completed: boolean;
-  content: string;
-  editing: boolean;
+  id: string; // id의 타입
+  completed: boolean; // todo 확인용
+  editing: boolean; // todo의 수정상태 확인
 }
